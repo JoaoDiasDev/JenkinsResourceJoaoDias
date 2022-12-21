d41712af9eb10844012ca3fc47d247fdc5b8a068
JoaoDias <joaodiasworking@gmail.com>
2022-12-20 21:41:40 -0300

    Finalizando curso!!!!!!!!!! Jenkins from Zero To Hero

---
+++ CourseJenkinsFromZeroToHeroLab/Scripts/Docker/docker-hub-push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "********************"
+echo "** Pushing image ***"
+echo "********************"
+
+IMAGE="jenkins-project"
+
+echo "** Logging in ***"
+docker login -u joaodias27 -p $PASS
+echo "*** Tagging image ***"
+docker tag $IMAGE:$BUILD_TAG joaodias27/$IMAGE:$BUILD_TAG
+echo "*** Pushing image ***"
+docker push joaodias27/$IMAGE:$BUILD_TAG
